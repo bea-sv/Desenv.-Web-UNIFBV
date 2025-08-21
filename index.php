74e6ada9699ac064415a1b8604cc45fd6e6d87b1
Beatriz de Souza <bea.souzav0@gmail.com>
2025-08-21 16:54:31 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+
+echo"beatriz";
+$nome = "beatriz";
+$dia = 21;
+$altura = 1.75;
+$ativo = true;
+$lista = ["php","mysql","html"];
+echo "\n$nome está no dia $dia na aula de {$lista[0]}.";
+
+
+#Estruturas de rep.
+$nota = 8.3;
+if ($nota>=7) {
+    echo "Aprovado\n";
+}else{
+    echo "Reprovado";
+}
+for ($i = 0; $i<3; $i++){
+    echo "for:$i\n";
+}
+?>
+
+
